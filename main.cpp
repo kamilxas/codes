d2da550d1bfbc54da649788a77fc541e60e54876
Moy_Gospodin <kamilxa200804@gmail.com>
2024-02-20 10:44:55 +0300

    first one

--- main.cpp
+++ main.cpp
@@ -10,24 +10,6 @@ ll ost(ll a1, ll a2) {
 }
 
 int main() {
-    int n;
-    cin >> n;
-    ll f = 1;
-    vector<ll> st;
-    while (f <= 1e18) {
-        st.push_back(f);
-        f *= 2;
-    }
-    for (int i = 0; i < n; i++) {
-        ll a, ans = 0;
-        cin >> a;
-        for (int j = 0; j < st.size() && st[j] <= a; j++) {
-            ll now = a - st[j] + 1, kv = now / st[j] / 2 * st[j] + min(st[j], now % (st[j] * 2));
-            ll fi = ost(kv, kv);
-            ll se = ost(fi, st[j]);
-            ans += se;
-        }
-        cout << ans % mod << endl;
-    }
+    ///Artem is a fag
     return 0;
 }
